861339cf0c67dba8d89385ac012ee94a0f251606
jrogovin <jrogovin@illinois.edu>
2024-04-09 16:36:11 -0500

    defined get-2023-and-earlier

--- hypothesis1.py
+++ hypothesis1.py
@@ -1,13 +1,25 @@
-
 import pandas as pd
 
 
-def process_datafile():
-    file = pd.read_csv('data/retractions.csv', usecols=['RetractionDate', 'RetractionNature'])
-    print(file)
+def get_retracted_articles(retraction_file):
+    file = pd.read_csv(retraction_file, usecols=['RetractionDate', 'RetractionNature'])
+    retracted_articles = file.loc[file['RetractionNature'] == 'Retraction']
+    return retracted_articles
 
+
+def get_2023_and_earlier(retracted_articles):
+    years = []
+    for datetime in retracted_articles['RetractionDate']:
+        date = datetime.split(' ')
+        day_month_year = date[0].split('/')
+        year = day_month_year[2]
+        years.append(int(year))
+    retracted_articles['RetractionYear'] = years
+
+    retracted_articles = retracted_articles.loc[retracted_articles['RetractionYear'] < 2024]
+    return retracted_articles
 
 
 if __name__ == '__main__':
-    process_datafile()
+    get_2023_and_earlier(get_retracted_articles('data/retractions.csv'))
 
