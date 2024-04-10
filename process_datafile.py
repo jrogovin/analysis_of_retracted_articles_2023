c366fbfd4d49eff3d3ce2b27fb5e8eda86eef986
jrogovin <jrogovin@illinois.edu>
2024-04-10 16:11:02 -0500

    completed process_datafile

--- process_datafile.py
+++ process_datafile.py
@@ -2,12 +2,23 @@ import pandas as pd
 
 
 def get_retracted_articles(retraction_file):
+    """
+    This function reads in a csv file with the columns "RetractionDate" and "RetractionNature" and returns only rows
+    where the retraction nature is retraction.
+    :param retraction_file: A csv file with retraction data
+    :return: All retracted articles from the dataset
+    """
     file = pd.read_csv(retraction_file, usecols=['RetractionDate', 'RetractionNature'])
     retracted_articles = file.loc[file['RetractionNature'] == 'Retraction']
     return retracted_articles
 
 
 def get_2023_and_earlier(retracted_articles):
+    """
+    This function takes retracted articles and returns only articles retracted in 2023 and earlier.
+    :param retracted_articles: A variable containing retracted articles
+    :return: All retracted articles retracted in 2023 and earlier.
+    """
     years = []
     for datetime in retracted_articles['RetractionDate']:
         date = datetime.split(' ')
