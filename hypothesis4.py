3b25ecf0920653cb4b8bc5a541f1f48376ed39ae
jrogovin <jrogovin@illinois.edu>
2024-04-26 12:14:06 -0500

    started hypothesis4.py.py

--- hypothesis4.py
+++ hypothesis4.py
@@ -8,9 +8,24 @@ def get_retracted_articles(retraction_file):
     :param retraction_file: A csv file with retraction data
     :return: All retracted articles from the dataset
     """
-    file = pd.read_csv(retraction_file, usecols=['Reason', 'RetractionDate', 'OriginalPaperDate'])
+    file = pd.read_csv(retraction_file, usecols=['Reason', 'RetractionDate', 'OriginalPaperDate', 'RetractionNature'])
     retracted_articles = file.loc[file['RetractionNature'] == 'Retraction']
     return retracted_articles
 
 
-def get_
+def convert_columns_to_datetime(retracted_articles):
+    """
+    This function converts the values in RetractionDate and OriginalPaperDate to datetime objects.
+    :param retracted_articles: A dataframe containing retracted articles
+    :return: Columns with dates as datetime objects
+    """
+    retracted_articles['RetractionDate'] = retracted_articles['RetractionDate'].apply(pd.to_datetime)
+    retracted_articles['OriginalPaperDate'] = retracted_articles['RetractionDate'].apply(pd.to_datetime)
+
+
+def get_time_between_dates(retracted_articles):
+
+
+
+if __name__ == '__main__':
+    convert_columns_to_datetime(get_date_range(get_retracted_articles('data/retractions.csv')))
